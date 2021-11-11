775d4d1c4071c1253252f638c794bd197b755cc5
Hieu <thanhhieu995@gmail.com>
2021-11-11 23:22:26 +0700

    Open MainActivity after 500 milisecond

--- app/src/main/java/com/example/documentsapi/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/documentsapi/ui/splash/SplashActivity.java
@@ -2,8 +2,11 @@ package com.example.documentsapi.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.documentsapi.MainActivity;
 import com.example.documentsapi.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +15,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 500);
     }
 }
