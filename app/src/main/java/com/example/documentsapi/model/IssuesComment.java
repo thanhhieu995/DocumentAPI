79768db949d575b200c75494f4ce192bab1e8846
Hieu <thanhhieu995@gmail.com>
2021-11-24 11:13:51 +0700

    Add IssuesComment class in model package

---
+++ app/src/main/java/com/example/documentsapi/model/IssuesComment.java
@@ -0,0 +1,10 @@
+package com.example.documentsapi.model;
+
+import java.io.Serializable;
+
+public class IssuesComment implements Serializable {
+    public String url;
+    public String issue_url;
+    public String node_id;
+    public User owner;
+}
--- app/src/main/java/com/example/documentsapi/ui/detailissues/DetailIssuesAdapter.java
+++ app/src/main/java/com/example/documentsapi/ui/detailissues/DetailIssuesAdapter.java
@@ -1,4 +1,33 @@
 package com.example.documentsapi.ui.detailissues;
 
-public class DetailIssuesAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DetailIssuesAdapter extends RecyclerView.Adapter<DetailIssuesAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from()
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
