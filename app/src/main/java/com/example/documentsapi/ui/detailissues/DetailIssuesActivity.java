786d98a19ac7d81c514eef6b8007dc0a2af09cdd
Hieu <thanhhieu995@gmail.com>
2021-11-24 10:50:11 +0700

    Create DetailIssuesActivity in detailissues package

---
+++ app/src/main/java/com/example/documentsapi/ui/detailissues/DetailIssuesActivity.java
@@ -0,0 +1,16 @@
+package com.example.documentsapi.ui.detailissues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.documentsapi.R;
+
+public class DetailIssuesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_issues);
+    }
+}
